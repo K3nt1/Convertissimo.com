52eb576c51c92c449c77c84cf14e1e2954e7ee2c
Vavan-13 <115944836+Vavan-13@users.noreply.github.com>
2023-05-03 14:40:46 +0200

    Add files via upload

---
+++ Convertion/Numération/dec_to_bin.py
@@ -0,0 +1,9 @@
+﻿def dec_to_bin(dec):
+    rep=''
+    while(dec>0):
+        if (dec%2==1):
+            rep='1'+rep
+        else:
+            rep='0'+rep
+        dec=dec//2
+    return(rep)
---
+++ Convertion/Numération/hexa_to_dec.py
@@ -0,0 +1,25 @@
+﻿def bin_to_dec(binaire):
+    l=len(binaire)
+    decimal=0
+    multi=1
+    for i in range(l):
+        if(binaire[l-i-1]=='1'):
+            decimal+=multi
+        multi*=2
+    return(decimal)
+
+def hexa_to_dec(hexa):
+    l=len(hexa)
+    decimal=0
+    multi=1
+    modele='0123456789ABCDEF'
+    for i in range(l):
+        chiffre=0
+        for j in range(16):
+            if(hexa[l-i-1]==modele[j]):
+                chiffre=j
+
+        decimal+=multi*chiffre
+        multi*=16
+    return(decimal)
+
